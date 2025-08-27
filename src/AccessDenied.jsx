9665cdea5c83325bf501fe4b7d640b9267090e32
Shruti H. <167879533+shruti-harayan@users.noreply.github.com>
2025-08-27 22:50:04 +0530

    Delete src/AccessDenied.jsx

--- src/AccessDenied.jsx
+++
@@ -1,9 +0,0 @@
-// AccessDenied.jsx
-export default function AccessDenied() {
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h2>Access Denied</h2>
-      <p>You do not have permission to view this page with your current account.</p>
-    </div>
-  );
-}
