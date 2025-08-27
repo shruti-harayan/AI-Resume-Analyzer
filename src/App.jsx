ef7275d16f6e562082a6c52a97c64d37efbff945
Shruti H. <167879533+shruti-harayan@users.noreply.github.com>
2025-08-27 22:49:37 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
 import StudentDashboard from "./pages/StudentDashboard";
 import RecruiterDashboard from "./pages/RecruiterDashboard";
+import HomePage from "./pages/HomePage";
 import ScoringTransparency from "./pages/ScoringTransparency";
+import Layout from "./components/Layout";
+import ProtectedRoute from "./components/Auth/ProtectedRoute";
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/student" element={<StudentDashboard />} />
-        <Route path="/recruiter" element={<RecruiterDashboard />} />
-        <Route path="/scoring-transparency" element={<ScoringTransparency />} />
-      </Routes>
-    </Layout>
+    <AuthProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/scoring-transparency" element={<ScoringTransparency />} />
+
+            {/* Protected Routes */}
+            <Route
+              path="/student"
+              element={
+                <ProtectedRoute role="student">
+                  <StudentDashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/recruiter"
+              element={
+                <ProtectedRoute role="recruiter">
+                  <RecruiterDashboard />
+                </ProtectedRoute>
+              }
+            />
+            
+          </Routes>
+        </Layout>
+      </Router>
+    </AuthProvider>
   );
 }
 
